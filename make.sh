6b1a585dac4bb349d571ea19111118dbf120e035
Yifeng Zhao <yifeng.zhao@rock-chips.com>
2021-10-14 14:51:12 +0800

    Add build scripts
    Please use "./make.sh" to build rk3588 uefi, and build rk3568 with
    "./make.sh rk3568". After the compilation is successful, you will
    get uboot_uefi.img in the current directory.
    
    Signed-off-by: Yifeng Zhao <yifeng.zhao@rock-chips.com>
    Signed-off-by: Chris Zhong <zyw@rock-chips.com>
    Change-Id: I41e5c7547925ecef7aff03bf6730e5e451b71c2f

---
+++ make.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+case "$1" in
+        rk3568 | RK3568 | 3568)
+                CHIP=3568;
+                ;;
+        *)
+                CHIP=3588
+                ;;
+esac
+echo Start to build rk$CHIP UEFI
+
+CMD=`realpath $0`
+COMMON_DIR=`dirname $CMD`
+TOP_DIR=$(realpath $COMMON_DIR/..)
+echo $TOP_DIR
+export GCC5_AARCH64_PREFIX=$TOP_DIR/prebuilts/gcc/linux-x86/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+export WORKSPACE=$TOP_DIR/uefi-monorepo
+export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms:$WORKSPACE/edk2-non-osi
+. edk2/edksetup.sh BaseTools &&
+make -C edk2/BaseTools &&
+build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/Rockchip/RK$CHIP/RK$CHIP.dsc &&
+echo "cd u-boot and re-packet uboot.img: ./scripts/fit-repack.sh -f uboot.img -d unpack/" &&
+cd ../u-boot &&
+mkdir -p unpack &&
+./make.sh rk$CHIP &&
+cp uboot.img uboot_uefi.img
+cp $WORKSPACE/Build/RK$CHIP/DEBUG_GCC5/FV/BL33_AP_UEFI.fd unpack/uboot &&
+./scripts/fit-repack.sh -f uboot_uefi.img -d unpack/
+mv uboot_uefi.img $WORKSPACE/uboot_uefi.img
